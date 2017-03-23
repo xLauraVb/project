6f6529eb9aaa38bab1238fa38f98f75a1b45f685
xLauraVb <vanbael.laura97@gmail.com>
2017-03-23 15:24:36 +0100

    Update and rename index.php to registratie.php

---
+++ registratie.php
@@ -0,0 +1,73 @@
+<?php
+
+if(!empty($_POST)) {
+// check of velden ingevuld zijn
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $options = [
+        'cost' =>12,
+
+    ];
+
+    $password = password_hash($password, PASSWORD_DEFAULT, $options);
+
+//connectie maken met database
+        try
+        {
+            $pdoconn = new PDO('mysql:host=localhost; dbname=', 'root', '');
+        }
+        catch(PDOException $e)
+        {
+
+        }
+
+// invoeren query
+    $query = "insert into users (email, password) values ('".$email."','".$password."') ";
+    $statement = $pdoconn->prepare("SELECT * from users where username = :username and
+password = :password");
+
+    $statement->bindParam(':email', $email);
+    $statement->bindParam(':password', $password);
+    $statement->execute();
+    $res = $pdoconn->query($query);
+
+    if ($res != false) {
+
+
+        session_start();
+        $_SESSION['email'] = $email;
+        header('Location: login.php');
+
+    } else {
+
+        header('Location: registratie.php');
+
+    }
+}
+
+?>
+       
+
+       <form action="" method="post">
+        
+        <fieldset class="fieldset_one">
+            
+            <legend>Welcome to spotify</legend>
+            <div>
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+            </div>
+        </fieldset>
+        
+        
+        <button type="submit" >Login</button>
+  
+        <div class="feedback"></div>
+        
+    </form>
+    
