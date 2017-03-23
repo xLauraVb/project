526477f5f2d2c2a61f3feec9d173d15ef0437160
xLauraVb <vanbael.laura97@gmail.com>
2017-03-23 15:40:26 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,41 @@
 <?php
 
+if(!empty($_POST)){
+
+//connectie database
+    try
+    {
+        $dbh = new PDO('mysql:host=localhost; dbname=', 'root', '');
+    }
+    catch(PDOException $e)
+    {
+
+    }
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $username = $_POST['username'];
+  
+
+// invoegen query
+    $sql = "SELECT * FROM users WHERE email = :email LIMIT 1";
+    $query = $dbh->prepare( $sql );
+    $query->execute( array( ':email'=>$email ) );
+    $results = $query->fetchAll( PDO::FETCH_ASSOC );
+
+    foreach( $results as $row ){
+        if(password_verify($password, $row['password'])){
+            session_start();
+            $_SESSION['email'] = $email;
+            header('Location: pinterest.php');
+        }
+        else
+        {
+            header('Location: login.php');
+        }
+    }
+
+}
 
 ?>
