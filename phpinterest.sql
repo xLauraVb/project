68107ed636b574620d6874a9f7899f2bf137097c
xLauraVb <vanbael.laura97@gmail.com>
2017-03-24 14:53:51 +0100

    Update phpinterest.sql

--- phpinterest.sql
+++ phpinterest.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.10
--- http://www.phpmyadmin.net
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Mar 23, 2017 at 03:39 PM
--- Server version: 5.5.42
--- PHP Version: 7.0.0
+-- Host: localhost
+-- Gegenereerd op: 24 mrt 2017 om 14:53
+-- Serverversie: 10.1.21-MariaDB
+-- PHP-versie: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,40 +17,78 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `phpinterest`
+-- Database: `phpopdracht`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Tabelstructuur voor tabel `gebruikers`
+--
+
+CREATE TABLE `gebruikers` (
+  `id` int(11) NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `gebruikers`
+--
+
+INSERT INTO `gebruikers` (`id`, `username`, `firstname`, `lastname`, `email`, `password`) VALUES
+(1, 'xLaura', 'laura', 'van bael', 'laura@gmail.com', '$2y$12$l2SSs0YQNgddkPsD6/jpk.RU3WA2ewNwzeSGNUE0GsdvANMbFAI8i'),
+(2, '', '', '', '', '$2y$12$EakNxEkrtvmAO8Fkl2454.fZ5HJnN0pvZ9EufKGTJnRjN4N9NeFum'),
+(3, '', '', '', '', '$2y$12$XUCmC5hk.j7KvZlSH.533eFt6y1vZhzDqh00leEYQo/i/xkeRQ2BC'),
+(4, '', '', '', '', '$2y$12$S5My9cdLyU96xrh2mRB5NOzzoXhKV06ZfqaGBQuP2gsmiQik0nco.'),
+(5, '', '', '', '', '$2y$12$jrMpJQI2lLCy1ntagfWB8OwCUOQOvimea98NRlhO.jEXevWdYbGNe'),
+(6, 'davy', 'davy', 'ceupens', 'davy@gmail.com', '$2y$12$TIll.UR3uygXZHKqovRJnOm9fzbT1LRyOlfQKsVTylbZNtfTi1vT.'),
+(7, 'xLaura', 'Laura', 'Van Bael', 'laura@gmail.com', '$2y$12$nm5FLZiEqdEM1dTcvU29IuDBC0Z/hTM3UEPbV/omuaHVper18NupK'),
+(8, 'xLaura', 'Laura', 'Van Bael', 'laura@gmail.com', '$2y$12$4ViPHBMckKp6.YNBXV6CLeSpH3J./9ZWR/2w4kCoMKrtP8q4ex48a'),
+(9, '', '', '', '', '$2y$12$oK9/rOF43vWzik7JvxXcbOvsPk3CMvT8ASOLRVFJKyMDy.DRfdAlm');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `email` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `firstname` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `lastname` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `username` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `email` varchar(200) NOT NULL,
+  `username` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Indexes for dumped tables
+-- Indexen voor geëxporteerde tabellen
 --
 
 --
--- Indexes for table `users`
+-- Indexen voor tabel `gebruikers`
+--
+ALTER TABLE `gebruikers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT voor geëxporteerde tabellen
 --
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT voor een tabel `gebruikers`
+--
+ALTER TABLE `gebruikers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT voor een tabel `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
