3e8f1d40bde8ea740632721461f6d667eff4628f
xLauraVb <vanbael.laura97@gmail.com>
2017-03-25 08:43:37 +0100

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -2,12 +2,17 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Homepage</title>
 </head>
 <body>
    
-   
-   sshjshshshjhjhj
+    <header>
+       <h1>"naam project"</h1>
+</header>
+
+<a href="profiel.php"><button>PROFIEL</button></a>
+<a href="logout.php"><button>UITLOGGEN</button></a>
+   <div id="achtergrond">
     
 </body>
 </html>
