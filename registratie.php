115c855dc078f923614049d64974bac4189aa023
davyceuppens <davy.ceuppens@hotmail.be>
2017-03-29 19:38:52 +0200

    Update registratie.php
    De code van min 6 aantal letters voor uw paswoord  zit er nu in, alleen weet ik niet hoe ik er zo'n error kan op plaatsen als het niet juist is zoals bij emailadres. Maar als het minder dan 6 letters is slaagt hij wel niet op in de databank dus dat is al iets

--- registratie.php
+++ registratie.php
@@ -1,57 +1,44 @@
 <?php
-
 if(!empty($_POST)) {
 // check of velden ingevuld zijn
-    $firstname = $_POST["firstname"];
-    $lastname = $_POST["lastname"];
-    $username = $_POST["username"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-
-    $options = [
-        'cost' =>12,
-
-    ];
-
-    $password = password_hash($password, PASSWORD_DEFAULT, $options);
-
-//connectie maken met database
-        try
-        {
-            $pdoconn = new PDO('mysql:host=localhost; dbname=phpopdracht', 'root', '');
+        if(strlen($_POST["password"]) >=6){
+            $firstname = $_POST["firstname"];
+            $lastname = $_POST["lastname"];
+            $username = $_POST["username"];
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            $options = [
+                'cost' =>12,
+            ];
+            $password = password_hash($password, PASSWORD_DEFAULT, $options);
+        //connectie maken met database
+                try
+                {
+                    $pdoconn = new PDO('mysql:host=localhost; dbname=phpopdracht', 'root', '');
+                }
+                catch(PDOException $e)
+                {
+                }
+        // invoeren query
+            $query = "insert into gebruikers (firstname, lastname, username, email, password) values ('".$firstname."','".$lastname."','".$username."','".$email."','".$password."') ";
+            $statement = $pdoconn->prepare("SELECT * from users where firstname = :firstname and lastname= :lastname and username = :username and email = :email and
+        password = :password");
+            $statement->bindParam(':email', $email);
+            $statement->bindParam(':password', $password);
+            $statement->bindParam(':firstname', $firstname);
+            $statement->bindParam(':lastname', $lastname);
+            $statement->bindParam(':username', $username);
+            $statement->execute();
+            $res = $pdoconn->query($query);
+            if ($res != false) {
+                session_start();
+                $_SESSION['email'] = $email;
+                header('Location: login.php');
+            } else {
+                header('Location: registratie.php');
+            }
         }
-        catch(PDOException $e)
-        {
-
-        }
-
-// invoeren query
-    $query = "insert into gebruikers (firstname, lastname, username, email, password) values ('".$firstname."','".$lastname."','".$username."','".$email."','".$password."') ";
-    $statement = $pdoconn->prepare("SELECT * from users where firstname = :firstname and lastname= :lastname and username = :username and email = :email and
-password = :password");
-
-    $statement->bindParam(':email', $email);
-    $statement->bindParam(':password', $password);
-    $statement->bindParam(':firstname', $firstname);
-    $statement->bindParam(':lastname', $lastname);
-    $statement->bindParam(':username', $username);
-    $statement->execute();
-    $res = $pdoconn->query($query);
-
-    if ($res != false) {
-
-
-        session_start();
-        $_SESSION['email'] = $email;
-        header('Location: login.php');
-
-    } else {
-
-        header('Location: registratie.php');
-
-    }
 }
-
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
