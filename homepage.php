8b5cfc165d55d1081a4c92259bb3259283d2c652
xLauraVb <vanbael.laura97@gmail.com>
2017-04-12 23:12:25 +0200

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -2,17 +2,30 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Homepage</title>
+    <title>IMDterest</title>
+    <link rel="stylesheet" href="style_homepage.css">
 </head>
 <body>
    
     <header>
-       <h1>"naam project"</h1>
+       <a id="logo" href="homepage.php"><h1>IMDTEREST</h1></a>
+       <a href="homepage.php"><button id ="buttonhomepage">HOMEPAGE</button></a>
+       <a href="profiel.php"><button id ="buttonprofiel">PROFIEL</button></a>
+<a href="logout.php"><button id ="buttonuitlog">UITLOGGEN</button></a>
 </header>
 
-<a href="profiel.php"><button>PROFIEL</button></a>
-<a href="logout.php"><button>UITLOGGEN</button></a>
-   <div id="achtergrond">
+<div id="achtergrond">
+   
+   <div id="titel">
+   <h1>Wat zijn jouw interesses?</h1>
+  </div>
+  
+  
+
+    
+    </div>
+
     
 </body>
+
 </html>
