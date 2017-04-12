8a86bb37e4a0d7936b40a22e5af95e7822c34f93
xLauraVb <vanbael.laura97@gmail.com>
2017-04-12 23:12:51 +0200

    Update profiel.php

--- profiel.php
+++ profiel.php
@@ -6,3 +6,37 @@
 ?> 
 
 // profiel aanpassen zie feature 4
+// foto + naam + username weergeven
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>IMDterest</title>
+    <link rel="stylesheet" href="style_homepage.css">
+</head>
+<body>
+   
+    <header>
+       <a id="logo" href="homepage.php"><h1>IMDTEREST</h1></a>
+       <a href="homepage.php"><button id ="buttonhomepage">HOMEPAGE</button></a>
+       <a href="profiel.php"><button id ="buttonprofiel">PROFIEL</button></a>
+<a href="logout.php"><button id ="buttonuitlog">UITLOGGEN</button></a>
+</header>
+
+<div id="achtergrond">
+   
+   <div id="bewerk">
+   <h1>Profiel bewerken</h1>
+  </div>
+  
+  
+
+    
+    </div>
+</body>
+</html>
+
+
